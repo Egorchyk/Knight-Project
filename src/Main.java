4a2c99df5a8b24ae83f33302a0828bb8f87976db
Anna Bakusheva <@gmail.com>
2022-05-25 20:59:07 +0300

    Create Menu

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Menu.MenuZero;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuZero.getMenu();
+        System.out.println("Proverka");
+    }
+}
---
+++ src/Menu/MenuZero.java
@@ -0,0 +1,39 @@
+package Menu;
+
+import java.util.Scanner;
+
+public class MenuZero {
+    public static void getMenu() {
+        System.out.println("Главное Меню");
+        System.out.println("1. Распечатайте статистику рыцаря");
+        System.out.println("2. Показать Снаряжение");
+        System.out.println("3. Изменить Снаряжение");
+        System.out.println("4. Сортировать Снаряжение");
+        System.out.println("5. Поиск Снаяржения");
+        System.out.println("6. Выход");
+        System.out.println("Выберите опцию: ");
+
+        int menuPosition = 0;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            menuPosition = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Можно вводить только числа\n");
+            getMenu();
+        }
+
+        switch(menuPosition) {
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("For Commit and Push");
+            case 4:
+            case 5:
+            case 6:
+            default:
+                System.out.println("Выбранной позиции нет в меню.");
+                System.out.println("Попробуйте еще раз!\n" );
+                getMenu();
+        }
+    }
+}
